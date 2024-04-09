4235f844d379aaa2c537a1bd7985249e83e23fe6
banuka <banukaramanayaka76@gmail.com>
2024-04-09 19:36:19 +0530

    feat:server init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
+import express from 'express'
+import 'dotenv/config'
+import http from 'http'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
 
+
+const app=express()
+
+app.use(cors({
+    credentials:true
+}))
+app.use(compression())
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+
+
+const port = process.env.PORT || 3000; 
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}!`);
+});
