404a9766d0ebfa2681130c3416f8bf1176d25ca2
banuka <banukaramanayaka76@gmail.com>
2024-04-10 12:03:53 +0530

    feat:create user record

--- srcMongo/routes/userRoutes.ts
+++ srcMongo/routes/userRoutes.ts
@@ -1,28 +1,26 @@
+import { register } from '../controllers/auth'
 import express from 'express'
-export const userRouter=express.Router()
+export const userRouter = express.Router()
 // get all users
-userRouter.get('/',(req,res)=>{
-    res.json({message:'get all users'})
-  })
-  
-  // get a single user
-  userRouter.get('/:id',(req,res)=>{
-    const {id}=req.params
-    res.json({message:'get a user',id})
-  })
-  
-  // post a user
-  userRouter.post('/',(req,res)=>{
-    const body=req.body
-    res.json({message:'post a user',body})
-  })
-  // delete a user
-  userRouter.delete('/:id',(req,res)=>{
-    const {id}=req.params
-    res.json({message:'delete a user',id})
-  })
-  // update a user
-  userRouter.patch('/:id',(req,res)=>{
-    const {id}=req.params
-    res.json({message:'update a user',id})
-  })
+userRouter.get('/', (req, res) => {
+    res.json({ message: 'get all users' })
+})
+
+// get a single user
+userRouter.get('/:id', (req, res) => {
+    const { id } = req.params
+    res.json({ message: 'get a user', id })
+})
+
+// post a user
+userRouter.post('/', register)
+// delete a user
+userRouter.delete('/:id', (req, res) => {
+    const { id } = req.params
+    res.json({ message: 'delete a user', id })
+})
+// update a user
+userRouter.patch('/:id', (req, res) => {
+    const { id } = req.params
+    res.json({ message: 'update a user', id })
+})
